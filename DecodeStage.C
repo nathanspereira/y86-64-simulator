22ff464aec76ceda6decc118c2dee055b251d64d
marymstephenson <stephensonml1@appstate.edu>
2025-04-03 16:38:19 -0400

    Implemented stages for phase2

--- DecodeStage.C
+++ DecodeStage.C
@@ -18,9 +18,9 @@ bool DecodeStage::doClockLow(PipeReg **pregs, Stage **stages) {
     D * dreg = (D *) pregs[DREG];
     E * ereg = (E *) pregs[EREG];
     
-    
-    uint64_t rA = RNONE, rB = RNONE;
-    setEinput(dreg, stat, icode, ifun, rA, rB, valC, valP);
+    uint64_t valA = 0, valB = 0;
+    uint64_t dstE = RNONE, dstM = RNONE, srcA = SAOK, srcB = SAOK;
+    setEinput(ereg, stat, icode, ifun, valC, valA, valB, dstE, dstM, srcA, srcB);
     return false;
 }
 
@@ -33,22 +33,24 @@ void DecodeStage::doClockHigh(PipeReg **pregs)
    ereg->getstat()->normal();
    ereg->geticode()->normal();
    ereg->getifun()->normal();
-   ereg->getrA()->normal();
-   ereg->getrB()->normal();
+   //ereg->getrA()->normal();
+   //ereg->getrB()->normal();
    ereg->getvalC()->normal();
-   ereg->getvalP()->normal();
 }
 
-void DecodeStage::setEinput(D * dreg, uint64_t stat, uint64_t icode, 
-                           uint64_t ifun, uint64_t rA, uint64_t rB,
-                           uint64_t valC, uint64_t valP)
+void DecodeStage::setEinput(E * ereg, uint64_t stat, uint64_t icode, 
+                           uint64_t ifun,
+                           uint64_t valC, uint64_t valA, uint64_t valB, uint64_t dstE, uint64_t dstM, uint64_t srcA, uint64_t srcB)
 {
-   dreg->getstat()->setInput(stat);
-   dreg->geticode()->setInput(icode);
-   dreg->getifun()->setInput(ifun);
-   dreg->getrA()->setInput(rA);
-   dreg->getrB()->setInput(rB);
-   dreg->getvalC()->setInput(valC);
-   dreg->getvalP()->setInput(valP);
+   ereg->getstat()->setInput(stat);
+   ereg->geticode()->setInput(icode);
+   ereg->getifun()->setInput(ifun);
+   ereg->getvalC()->setInput(valC);
+   ereg->getvalA()->setInput(valA);
+   ereg->getvalB()->setInput(valB);
+   ereg->getdstE()->setInput(dstE);
+   ereg->getdstM()->setInput(dstM);
+   ereg->getsrcA()->setInput(srcA);
+   ereg->getsrcB()->setInput(srcB);
 }
 
--- MemoryStage.C
+++ MemoryStage.C
@@ -14,10 +14,37 @@
 
 
 bool MemoryStage::doClockLow(PipeReg **pregs, Stage **stages) {
+
+    M * Mreg = (M *) pregs[MREG];
+    W * Wreg = (W *) pregs[WREG];
+
+    uint64_t valM = RNONE;
+    setMinput(wreg, stat, icode, valE, valM, dstE, dstM);
     return false;
 }
 
-void MemoryStage::doClockHigh(PipeReg **pregs) {
+void MemoryStage::doClockHigh(PipeReg **pregs)
+{
 
+   M * mreg = (M *) pregs[MREG];
+
+   mreg->getpredPC()->normal();
+   wreg->getstat()->normal();
+   wreg->geticode()->normal();
+   wreg->getvalE()->normal();
+   wreg->getvalM()->normal();
+   wreg->getdstE()->normal();
+   wreg->getdstM()->normal();
+}
+
+void MemoryStage::setMinput(W * wreg, uint64_t stat, uint64_t icode,
+                           uint64_t valE, uint64_t valM, uint64_t dstE, uint64_t dstM)
+{
+   wreg->getstat()->setInput(stat);
+   wreg->geticode()->setInput(icode);
+   wreg->getvalE()->setInput(valE);
+   wreg->getvalM()->setInput(valM);
+   wreg->getdstE()->setInput(dstE);
+   wreg->getdstM()->setInput(dstM);
 }
 
